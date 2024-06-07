c86cf611e926c6d392d54bc58f2f6cd4e1a35509
Luciano <luciano.oliveira96@yahoo.com.br>
2024-06-06 21:47:01 -0300

    atualização 050624

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer>
+        <div>
+            <img src="./assets/images/capturaFace.png" alt="">
+        </div>
+        <div>
+            <h2>Poliana Pavan</h2>
+            <p>Um espaço para pessoas que buscam uma vida mais leve ✨
+                Terapeuta cognitivo-comportamental | CRP 06/167763</p>
+        </div>
+        <div id="redes-sociais">
+            <div class="redes"><img src="./assets/images/instagram-40.png" alt=""></div>
+            <div class="redes"><a rel="stylesheet" href="https://wa.me/+5519994536199?text=Olá,%20gostaria%20de%20agendar%20uma%20consulta" target="_blank" text="olá "><img src="./assets/images/whatsapp-33.png" alt=""></a></div>
+            <div class="redes"><img src="./assets/images/linkedin.png" alt=""></div>
+        </div>
+    </footer>
---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+        <h1>Poliana Pavan</h1>
+        <nav>
+            <ul>
+                <li><a href="/">Início</a></li>
+                <li><a href="clinica.html">A Clínica</a></li>
+                <li><a href="valores.html">Valores</a></li>
+                <!-- <li><a href="contato.html">Contato</a></li> -->
+            </ul>
+        </nav>
+        <aside><a href="contato.html">Agende sua sessão</a></aside>
+    </header>
