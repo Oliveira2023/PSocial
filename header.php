7145186e086bdea296c78542094c5c1840fb4581
Luciano <luciano.oliveira96@yahoo.com.br>
2024-06-29 19:21:58 -0300

    update290624

--- header.php
+++ header.php
@@ -1,12 +1,13 @@
 <header>
-        <h1><a href="index.html">Poliana Pavan</a></h1>
-        <nav>
-            <ul>
-                <li><a href="index.html">Início</a></li>
-                <li><a href="projeto.html">O projeto</a></li>
-                <li><a href="valores.html">Valores</a></li>
-                <!-- <li><a href="contato.html">Contato</a></li> -->
-            </ul>
-        </nav>
-        <aside><a href="contato.html">Agende sua sessão</a></aside>
+    <div id="logo"><img src="./assets/images/logo300.jpg" alt="" srcset=""></div>
+    <h1><a href="index.html">ClickMind</a></h1>
+    <nav>
+        <ul>
+            <li><a href="index.html">Início</a></li>
+            <li><a href="projeto.html">O projeto</a></li>
+            <li><a href="valores.html">Valores</a></li>
+            <!-- <li><a href="contato.html">Contato</a></li> -->
+        </ul>
+    </nav>
+    <aside><a href="contato.html">Agende sua sessão</a></aside>
     </header>
